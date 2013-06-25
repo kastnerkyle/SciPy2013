0fbbf9e8aed45940a1016641ce52efd13e202856
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-06-25 13:30:40 -0500

    Working plots for classification example

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.optimize as opt
 from scipy.io import loadmat
 import matplotlib.pyplot as plot
+from matplotlib import cm
 
 def sigmoid(X):
     return 1./(1.+np.exp(-X))
@@ -46,18 +47,35 @@ def logistic_cost_reg_grad(theta, X, y, l=.1):
 
 from sklearn import datasets
 digits = datasets.load_digits()
+X = digits.data
+plotdim = 3
+randind = np.random.randint(X.shape[0],size=(plotdim,plotdim))
+f1, axarr = plot.subplots(plotdim,plotdim)
+for i in range(plotdim):
+    for j in range(plotdim):
+        axarr[i,j].get_xaxis().set_visible(False)
+        axarr[i,j].get_yaxis().set_visible(False)
+#Digits are 8x8 images
+for i in range(plotdim):
+    for j in range(plotdim):
+        axarr[i,j].imshow(X[randind[i,j],:].reshape(8,8), cmap=cm.gray)
+
 #These digits are randomly ordered, if given an ORDERED set
 #of training data make sure to randomize before splitting
 #Normalize between 0 and 1
 X = digits.data/255.
+
 #Subtract the mean, though it doesn't appear to make a big difference
 X = X - np.mean(X)
 y = digits.target.T[:,np.newaxis]
+
 #Add bias terms
 X = np.hstack((np.ones((X.shape[0],1)), X))
+
 #Expect 10 labels for digits
 num_labels = np.amax(y)-np.amin(y)+1
 all_theta = np.zeros((X.shape[1],num_labels))
+
 for c in range(num_labels):
     #Initialize theta to zeros and pass into optimization routine
     theta0 = np.zeros((X.shape[1],))
@@ -73,3 +91,13 @@ h = sigmoid(np.dot(X, all_theta))
 pred = np.argmax(h,axis=1)[:,np.newaxis]
 print "Classification accuracy(%):" + `100*np.sum((y == pred))/float(len(y))`
 
+for i in range(plotdim):
+    for j in range(plotdim):
+        ind = randind[i,j]
+        if y[ind] == pred[ind]:
+            text_color = 'g'
+        else:
+            text_color = 'r'
+        axarr[i,j].set_title("Label=" + `pred[ind,:][0]`,color=text_color)
+plot.suptitle("Overall classification accuracy(%):" + `round(100*np.sum((y == pred))/float(len(y)),2)`)
+plot.show()
--- matrix_inverse_linear_regression.py
+++ matrix_inverse_linear_regression.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plot
-import math
 
 
-N = 10
-noise_var = B = 5
-xs = np.matrix(range(N)).T
+N = 100
+upper_lim = 10 #Upper bound for data, where points will be random from 0 to upper_lim
+noise_var = B = 4 #Add Gaussian noise
+xs = np.sort(upper_lim*np.random.rand(N,1), axis=0)
 ys = np.square(xs) - 4*xs + 1
-wm = ys + np.sqrt(B)*np.random.randn(N,1)
+wm = ys + B*np.random.randn(N,1)
 
 def gen_dft(m, n, N):
     return np.exp(1j*-2*m*n/N)
@@ -17,20 +17,23 @@ def gen_polynomial(x, m):
     return np.power(x, m)
 
 N_basis = 3
-#basis_func = np.vectorize(gen_dft)
-#basis = basis_func(xs, np.arange(N_basis), N).T
 basis_func = np.vectorize(gen_polynomial)
-basis = basis_func(xs, np.arange(N_basis)).T
-test_data = t = basis*wm
+basis = basis_func(np.arange(N)[:,np.newaxis], np.arange(N_basis)).T
+
+#Uncomment these lines lines to use Fourier basis instead
+#basis_func = np.vectorize(gen_dft)
+#basis = basis_func(np.arange(N)[:,np.newaxis], np.arange(N_basis),N).T
+
+test_data = t = np.dot(basis,wm)
+
 #Calculate the Moore-Penrose pseudoinverse using the following formula
 #maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
 #Direct calculation appears to have numerical instability issues...
 #Luckily the pinv method calculates Moore-Penrose pseudo inverse using SVD, which largely avoids the numerical issues
-maximum_likelihood = wml = np.linalg.pinv(basis)*t
+maximum_likelihood = wml = np.dot(np.linalg.pinv(basis),t)
 
 plot.figure()
 plot.title("Regression fit using polynomial basis function, number of basis functions = $" + `N_basis` + "$")
-plot.plot(ys, 'b')
-plot.plot(wm, 'ro')
-plot.plot(np.real(wml), 'g')
+plot.plot(wm, 'rx')
+plot.plot(np.real(wml), 'b')
 plot.show()
